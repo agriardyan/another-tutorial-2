e2675b097b0f453ceec59506a9f46ed09c9b1232
Agustinus Agri Ardyan <Agustinus_A7234@mitrais.com>
2018-02-23 15:11:54 +0800

    first commit

---
+++ src/main/java/com/soldev/reporterfront/MyBasicAuthenticationEntryPoint.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.soldev.reporterfront;
+
+/**
+ *
+ * @author Agustinus_A7234
+ */
+public class MyBasicAuthenticationEntryPoint {
+    
+}
---
+++ src/main/java/com/soldev/reporterfront/ReporterfrontApplication.java
@@ -0,0 +1,13 @@
+package com.soldev.reporterfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporterfrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReporterfrontApplication.class, args);
+        
+    }
+}
